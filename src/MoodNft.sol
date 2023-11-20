b3aa617481e722bc173002cae4b3bf64abca542f
hhhfriends19 <84479916@qq.com>
2023-11-20 18:02:34 +0800

    modify MoodNft.sol

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,5 +5,27 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MoodNft is ERC721 {
-    constructor() ERC721("Mood NFT", "MN") {}
+    uint256 private s_tokenCounter;
+    string private s_sadSvg;
+    string private s_happySvg;
+
+    constructor(
+        string memory sadSvg,
+        string memory happySvg
+    ) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+    }
+
+    // everyone can mint this nft
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    // define what the nft looks like
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
 }
